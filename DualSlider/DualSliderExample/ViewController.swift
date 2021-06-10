57797b0447ba31b4e31b88affcc07cfda9e8069d
vtmonilgandhi <monil.gandhi@volansystech.com>
2021-06-10 10:17:06 +0530

    Add dual slider projrct

---
+++ DualSlider/DualSliderExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DualSliderExample
+//
+//  Created by Monil Gandhi on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
